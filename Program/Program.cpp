64f014b717f6a92f319ef1f441b213030a2e5aa6
Kihyeon <kimkijang1@naver.com>
2024-11-12 21:46:04 +0900

    (2024.11.12) 단일 연결 리스트

---
+++ Program/Program.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+
+template <typename T>
+class SingleLinkedList
+{
+private:
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    int size;
+    Node* head;
+
+public:
+    SingleLinkedList()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    ~SingleLinkedList()
+    {
+        Node* current = head;
+        while (current != nullptr)
+        {
+            Node* nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+    }
+
+    void pushFront(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+
+        size++;
+    }
+};
+
+int main()
+{
+    SingleLinkedList<int> singleLinkedList;
+
+    singleLinkedList.pushFront(10);
+    singleLinkedList.pushFront(20);
+}
