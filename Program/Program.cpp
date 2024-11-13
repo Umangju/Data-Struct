3ebb2e9004d911bfc4b3640cbbd22965b98d8421
Kihyeon <kimkijang1@naver.com>
2024-11-13 21:47:04 +0900

    (2024.11.13) 양방향 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 template <typename T>
 class SingleLinkedList
 {
@@ -8,6 +10,7 @@ private:
     {
         T data;
         Node* next;
+
     };
 
     int size;
@@ -49,7 +52,95 @@ public:
 
         size++;
     }
+
+    void popFront()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+            head = deleteNode->next;
+            delete deleteNode;
+            size--;
+        }
+    }
+
+    void pushBack(T data)
+    {
+        if (head == nullptr)
+        {
+            head = new Node;
+
+            head->data = data;
+            head->next = nullptr;
+        }
+        else
+        {
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            Node* newNode = new Node;
+
+            currentNode->next = newNode;
+
+            newNode->data = data;
+            newNode->next = nullptr;
+        }
+
+        size++;
+    }
+
+    void popBack()
+    {
+        if (head == nullptr) // 리스트가 비어 있는 경우
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+
+        if (head->next == nullptr) // 노드가 하나만 있는 경우
+        {
+            delete head;
+            head = nullptr;
+            // head->next;
+        }
+        else // 노드가 두 개 이상 있는 경우
+        {
+            Node* previousNode = nullptr;
+            Node* deleteNode = head;
+
+            while (deleteNode->next != nullptr)
+            {
+                previousNode = deleteNode;
+                deleteNode = deleteNode->next;
+            }
+
+            previousNode->next = nullptr;
+            delete deleteNode;
+        }
+
+        size--;
+    }
+
+    void show() const
+    {
+        Node* currentNode = head;
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+            currentNode = currentNode->next;
+        }
+        cout << endl;
+    }
 };
+
+
 
 int main()
 {
@@ -57,4 +148,17 @@ int main()
 
     singleLinkedList.pushFront(10);
     singleLinkedList.pushFront(20);
+
+    singleLinkedList.pushBack(5);
+    singleLinkedList.pushBack(0);
+
+    singleLinkedList.show();
+
+    singleLinkedList.popBack();
+    singleLinkedList.show();
+
+    singleLinkedList.popFront();
+    singleLinkedList.popFront();
+
+    return 0;
 }
