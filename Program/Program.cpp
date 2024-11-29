24dd4e2b288043b66115b8b35172b8fbcd98b717
Kihyeon <kimkijang1@naver.com>
2024-11-29 21:45:38 +0900

    (2024.11.29) 편향 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -33,6 +33,29 @@ public:
 		}
 	}
 
+	~HashTable()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			Node* deleteNode = bucket[i].head;
+			Node* nextNode = bucket[i].head;
+
+			if (bucket[i].head == nullptr)
+			{
+				continue;
+			}
+			else
+			{
+				while (nextNode != nullptr)
+				{
+					nextNode = deleteNode->next;
+					delete deleteNode;
+					deleteNode = nextNode;
+				}
+			}
+		}
+	}
+
 	template <typename T>
 	int HashFunction(T key)
 	{
@@ -91,11 +114,96 @@ public:
 		bucket[hashIndex].count++;
 	}
 
+	void Remove(KEY key)
+	{
+		// 해시 인덱스를 계산하여 해당 버킷을 찾음
+		int hashIndex = HashFunction(key);
+
+		// 현재 버킷의 첫 번째 노드를 가져옴
+		Node* currentNode = bucket[hashIndex].head;
+		Node* previousNode = nullptr;
+
+		// 버킷이 비어있는 경우
+		if (currentNode == nullptr)
+		{
+			cout << "Not Key Found." << endl;
+			return;
+		}
+		else
+		{
+			// 연결 리스트를 순회하며 노드를 탐색
+			while (currentNode != nullptr)
+			{
+				if (currentNode->key == key) // 일치하는 키를 발견한 경우
+				{
+					if (previousNode == nullptr) // 첫 번째 노드라면
+					{
+						bucket[hashIndex].head = currentNode->next;
+					}
+					else // 중간 또는 마지막 노드라면
+					{
+						previousNode->next = currentNode->next;
+					}
+
+					// 노드 삭제
+					delete currentNode;
+
+					// 버킷 노드 개수 감소
+					bucket[hashIndex].count--;
+
+					cout << "Key " << key << " removed successfully." << endl;
+					return;
+				}
+				else // 일치하는 키를 찾지 못한 경우 다음 노드로 이동
+				{
+					previousNode = currentNode;
+					currentNode = currentNode->next;
+				}
+			}
+
+			// 키를 찾지 못한 경우
+			cout << "Key " << key << " not found in the hash table." << endl;
+		}
+	}
+
+	void Show()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			// 현재 버킷의 head를 가져옴
+			Node* currentNode = bucket[i].head;
+
+			cout << "Bucket [" << i << "]: ";
+
+			// 버킷이 비어있으면 출력
+			if (currentNode == nullptr)
+			{
+				cout << "Empty" << endl;
+				continue;
+			}
+
+			// 연결 리스트 순회
+			while (currentNode != nullptr)
+			{
+				cout << "(" << currentNode->key << ", " << currentNode->value << ") -> ";
+				currentNode = currentNode->next;
+			}
+			cout << "nullptr" << endl;
+		}
+	}
+
 };
 
 int main()
 {
 	HashTable<int, std::string> hashTable;
+
+	hashTable.Insert(10, "Vector");
+	hashTable.Insert(15, "Stack");
+	hashTable.Insert(6, "Queue");
+	hashTable.Insert(20, "Linked List");
+
+	hashTable.Show();
 
 	return 0;
 }
