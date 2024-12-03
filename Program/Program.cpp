c4149dc79e159f9151c1fb7755c43ad93d40f2ec
Kihyeon <kimkijang1@naver.com>
2024-12-03 21:46:24 +0900

    (2024.12.03) 그래프(인접 행렬)

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,118 +2,36 @@
 
 using namespace std;
 
-#define SIZE 8
+#define SIZE 10
 
 template<typename T>
-class Heap
+class AdjacencyMatrix
 {
 private:
-    int index;
-    T container[SIZE];
+    int size;               // 정점의 개수
+    T vertex[SIZE];         // 정점의 집합
+    int matrix[SIZE][SIZE]; // 인접 행렬
 
 public:
-    Heap()
+    AdjacencyMatrix()
     {
+        size = 0;
+
         for (int i = 0; i < SIZE; i++)
         {
-            container[i] = 0;
-        }
+            vertex[i] = NULL;
 
-        index = 0;
-    }
-
-    void Insert(T data)
-    {
-        if (index >= SIZE)
-        {
-            cout << "Heap overflow." << endl;
-        }
-        else
-        {
-            container[index] = data;
-
-            int child = index;
-            int parent = child / 2;
-
-            while (child > 1)
+            for (int j = 0; j < SIZE; ++j)
             {
-                if (container[parent] < container[child])
-                {
-                    swap(container[parent], container[child]);
-                }
-
-                child = parent;
-                parent = child / 2;
-            };
-        }
-    }
-
-    T Remove()
-    {
-        if (index == 0)
-        {
-            cout << "Heap underflow." << endl;
-            return -1;
-        }
-
-        T removed = container[1];
-        container[1] = container[index];
-        container[index] = 0;
-        index--;
-
-        int parent = 1;
-        while (true)
-        {
-            int left = parent * 2;
-            int right = parent * 2 + 1;
-            int largest = parent;
-
-            if (left <= index && container[left] > container[largest])
-            {
-                largest = left;
+                matrix[i][j] = NULL;
             }
-
-            if (right <= index && container[right] > container[largest])
-            {
-                largest = right;
-            }
-
-            if (largest == parent)
-            {
-                break;
-            }
-
-            T temp = container[parent];
-            container[parent] = container[largest];
-            container[largest] = temp;
-
-            parent = largest;
         }
-
-        return removed;
     }
-
-    void Show()
-    {
-        for (int i = 0; i <= index; i++)
-        {
-            cout << container[i] << " ";
-        }
-        cout << endl;
-    }
-
 };
 
 int main()
 {
-    Heap<int> heap;
-
-    heap.Insert(5);
-    heap.Insert(7);
-    heap.Insert(9);
-    heap.Insert(16);
-
-    heap.Show();
+    AdjacencyMatrix<int> adjacencyMatrix;
 
     return 0;
 }
